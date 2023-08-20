8a3e618c2828544d95d88f5c81e9e361708f6154
chiroptical <chiroptical@gmail.com>
2023-08-20 17:48:05 -0400

    Remove custom text version

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}:
-let
-  haskell-text = pkgs.callPackage ./haskell-text.nix {};
-in
+# let
+#   haskell-text = pkgs.callPackage ./haskell-text.nix {};
+#in
   pkgs.haskell.packages.ghc94.extend (final: prev: {
-    "text" = final.callCabal2nix "text" haskell-text {};
   })
